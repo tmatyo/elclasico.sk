12b77124f39db240174ecc54772f6ab11acedefb
Tamás Matyó <tomi.matyo@gmail.com>
2024-05-21 00:49:39 +0200

    feat(fixtures): fixtures updated and link updated to semantic HTML instead of js click event

--- src/templates/Fixtures.js
+++ src/templates/Fixtures.js
@@ -1,58 +1,45 @@
-import React, { Component } from "react";
 import "../css/fixtures.css";
+import { useEffect, useState } from "react";
 
-export default class Fixtures extends Component {
-	constructor() {
-		super();
-		this.state = {
-			fx: [],
-		};
-	}
+export default function Fixtures() {
+	const [fix, setFix] = useState([]);
 
-	componentDidMount() {
-		//this.setState({fx:fx});
+	useEffect(() => {
 		let v = new Date();
 		fetch("/fixtures.json?v=" + v.getTime())
 			.then((res) => res.json())
-			.then((res) => this.setState({ fx: res }))
+			.then((res) => setFix(res))
 			.catch((error) => console.log("ERROR:", error));
-	}
+	});
 
-	goTo(link) {
-		window.open(link, "noopener");
-
-		this.goTo = this.goTo.bind(this);
-	}
-
-	render() {
-		return (
-			<div id="fixtures">
-				<h2>Hralo sa</h2>
-				<div className="timeline">
-					{this.state.fx.map((row, index) => {
-						return (
-							<div
-								key={index}
-								className={"timeline-item " + row.winner + (index % 2 ? " even" : " odd")}
-								title={row.event}
-							>
-								<div className={index % 2 ? "even-line" : ""}>
-									{index % 2 ? <span className="time">{row.time}</span> : ""}
-								</div>
-								<div
-									className={"timeline-fixture " + (index % 2 ? "even" : "odd")}
-									onClick={() => this.goTo(row.link)}
-								>
-									<span>{row.home_team}</span> <span>{row.score}</span> <span>{row.away_team}</span>
-								</div>
-								<div className={index % 2 ? "" : "odd-line"}>
-									{index % 2 ? "" : <span className="time">{row.time}</span>}
-								</div>
+	return (
+		<div id="fixtures">
+			<h2>Hralo sa</h2>
+			<div className="timeline">
+				{fix.map((row, index) => {
+					return (
+						<div
+							key={index}
+							className={"timeline-item " + row.winner + (index % 2 ? " even" : " odd")}
+							title={row.event}
+						>
+							<div className={index % 2 ? "even-line" : ""}>
+								{index % 2 ? <span className="time">{row.time}</span> : ""}
 							</div>
-						);
-					})}
-				</div>
+							<a
+								href={row.link}
+								target={"_blank"}
+								className={"timeline-fixture " + (index % 2 ? "even" : "odd")}
+							>
+								<span>{row.home_team}</span> <span>{row.score}</span> <span>{row.away_team}</span>
+							</a>
+							<div className={index % 2 ? "" : "odd-line"}>
+								{index % 2 ? "" : <span className="time">{row.time}</span>}
+							</div>
+						</div>
+					);
+				})}
 			</div>
-		);
-	}
+		</div>
+	);
 }
